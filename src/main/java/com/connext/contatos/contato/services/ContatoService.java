d055737876363c993ea4eeb853a10fda106aa1bf
Artur Henrique Cruz da Silva <arturcruz2505@gmail.com>
2025-07-05 16:25:21 -0300

    Removing unused import

--- src/main/java/com/connext/contatos/contato/services/ContatoService.java
+++ src/main/java/com/connext/contatos/contato/services/ContatoService.java
@@ -1,7 +1,7 @@
 package com.connext.contatos.contato.services;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
